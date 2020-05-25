4bc9c4c59c263e6050999b61c80499cf305c6fe5
Strategistatwork <forwardpatterns@gmail.com>
2020-05-25 10:25:33 +0530

    Commit on tap code

--- pyseven/pyseven/__init__.py
+++ pyseven/pyseven/__init__.py
@@ -1,2 +1,18 @@
+from email._header_value_parser import get_phrase
 print("Hello World")
 print("I am the Strategist")
+phrase="Don't Panic!"
+plist=list(phrase)
+print(get_phrase)
+print(plist)
+# Changing don't panic in to on tap
+for i in range(4):
+    plist.pop()
+plist.remove("'")
+plist.remove("D") 
+plist.extend([plist.pop(), plist.pop()]) 
+plist.insert(2, " ")
+plist.pop(4) 
+new_phrase="".join(plist)
+print(plist)
+print(new_phrase)
