a65af916cdba7ee505743645c133722d98eeb037
Strategistatwork <forwardpatterns@gmail.com>
2020-06-07 10:26:38 +0530

    commit vowels

--- pyseven/pyseven/__init__.py
+++ pyseven/pyseven/__init__.py
@@ -1,9 +1,9 @@
 from email._header_value_parser import get_phrase
 print("Hello World")
 print("I am the Strategist")
-phrase="Don't Panic!"
+phrase="Don't panic!"
 plist=list(phrase)
-print(get_phrase)
+print(phrase)
 print(plist)
 # Changing don't panic in to on tap
 for i in range(4):
@@ -16,3 +16,11 @@ plist.pop(4)
 new_phrase="".join(plist)
 print(plist)
 print(new_phrase)
+vowels=['a','e','i','o','u']
+word=input('Enter a word to search for vowels')
+found=[]
+for letter in word:
+    if letter in vowels:
+        if letter not in found:
+            found.append(letter)
+print(found)        
